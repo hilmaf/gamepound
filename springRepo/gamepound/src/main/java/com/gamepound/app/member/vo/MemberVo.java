a1add528e7e2bfb647a7e772e3810d4aebaece9d
hdelay <102139789+hdelay@users.noreply.github.com>
2023-12-31 12:25:04 +0900

    [feat] 로그인 기능 추가, loginMember session저장 - Member Controller, Service, Dao, Vo, mapper파일 추가

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/member/vo/MemberVo.java
@@ -0,0 +1,19 @@
+package com.gamepound.app.member.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVo {
+	
+	private String no;
+	private String name;
+	private String email;
+	private String pwd;
+	private String pic;
+	private String intro;
+	private String siteUrl;
+	private String enrollDate;
+	private String updateDate;
+	private String quitYn;
+	
+}
