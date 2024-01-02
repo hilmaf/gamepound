46d6831aceb5f3f7acdd177ea6f8deccaf79d784
orlando <glee1470@naver.com>
2024-01-02 14:31:20 +0900

    [file] 후원 service, dao 파일

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/back/dao/BackDaoLKM.java
@@ -0,0 +1,13 @@
+package com.gamepound.app.back.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.gamepound.app.back.vo.BackVo;
+
+public class BackDaoLKM {
+
+//	public int back(SqlSessionTemplate sst, BackVo vo) {
+//		return sst.insert();
+//	}
+
+}
