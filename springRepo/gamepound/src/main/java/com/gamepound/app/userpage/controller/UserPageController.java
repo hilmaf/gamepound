e4561c65f31c4d6a5fa74c9c42d3374b972f5003
orlando <glee1470@naver.com>
2024-01-02 22:19:10 +0900

    [feat] 리뷰 목록 조회 기능 구현 중

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/userpage/controller/UserPageController.java
@@ -0,0 +1,30 @@
+package com.gamepound.app.userpage.controller;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gamepound.app.userpage.service.UserPageService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("review")
+public class UserPageController {
+	
+	private final UserPageService service;
+	
+	// 특정 창작자에게 달린 리뷰 목록 조회하기
+//	@GetMapping("list")
+//	public Map<String, Object> listReview(String memberNo) {
+//		
+//		Map<String, Object> map = service.listReview(memberNo);
+//		
+//		return map;
+//	}
+//	
+	
+}
