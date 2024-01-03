5778b57a81526a82e4fa19139decfb3f3df54fd9
DESKTOP-65C7M8P\user1 <clsrnclsrn1111@naver.com>
2024-01-03 17:17:29 +0900

    [feat] 카테고리 목록 조회 기능 구현 중

--- springRepo/gamepound/src/main/java/com/gamepound/app/project/controller/ProjectControllerHYJ.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/controller/ProjectControllerHYJ.java
@@ -1,9 +1,14 @@
 package com.gamepound.app.project.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gamepound.app.project.ProjectAchievementRate;
 import com.gamepound.app.project.service.ProjectServiceHYJ;
+import com.gamepound.app.project.vo.ProjectVo;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +18,30 @@ import lombok.RequiredArgsConstructor;
 public class ProjectControllerHYJ {
 	private final ProjectServiceHYJ service;
 	
-	//프로젝트 조회
+	//목록 조회 - 카테고리(대분류)
+	@GetMapping("list/category")
+	public void projectListCategory() {
+		List<ProjectVo> voList = service.projectListCategory();
+		
+		//TODO-HYJ : ajaxㄱㄱ
+		for (ProjectVo vo : voList) {
+			System.out.println(vo);
+		}
+	}
+	
+	//목록 조회 - 카테고리(소분류)
+	
+	//목록 조회 - 인기순
+	
+	
+	//목록 조회 - 신규
+	
+	
+	//목록 조회 - 마감임박
+	
+	
+	//목록 조회 - 공개예정
+	
+	
 	
 }
--- springRepo/gamepound/src/main/java/com/gamepound/app/project/service/ProjectServiceHYJ.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/service/ProjectServiceHYJ.java
@@ -1,9 +1,12 @@
 package com.gamepound.app.project.service;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Service;
 
 import com.gamepound.app.project.dao.ProjectDaoHYJ;
+import com.gamepound.app.project.vo.ProjectVo;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +15,9 @@ import lombok.RequiredArgsConstructor;
 public class ProjectServiceHYJ {
 	private final ProjectDaoHYJ dao;
 	private final SqlSessionTemplate sst;
+	
+	//목록 조회 - 카테고리
+	public List<ProjectVo> projectListCategory() {
+		return dao.projectListCategory(sst);
+	}
 }
