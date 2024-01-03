99d0ef9fc1f76f17046249ef7c1dc810a35df359
hdelay <102139789+hdelay@users.noreply.github.com>
2024-01-03 20:26:35 +0900

    [file] 선물RewardVo 작성 및 mapper파일 추가

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/reward/vo/RewardVo.java
@@ -0,0 +1,11 @@
+package com.gamepound.app.reward.vo;
+
+import lombok.Data;
+
+@Data
+public class RewardVo {
+	private String no;
+	private String projectNo;
+	private String amount;
+	private String name;
+}
