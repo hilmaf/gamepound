a1c32621052eb6d947a7607dd6d455946a6bb931
orlando <glee1470@naver.com>
2024-01-03 16:47:17 +0900

    [feat] 마감기한 계산

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectRemainingPeriod.java
@@ -0,0 +1,29 @@
+package com.gamepound.app.project;
+
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ProjectRemainingPeriod {
+	
+	// 마감기한으로부터 D-
+	public static String getRemainingPeriod(String endDate) {
+		String diff = null;
+	    try {
+	    	// 날짜 객체
+	    	DateFormat df = new SimpleDateFormat("yy/MM/dd");
+	    	
+	    	// 마감일, sysdate 간 차이
+	    	Date currentDate = new Date();
+		    Date endDate_ = df.parse(endDate);
+		    
+		    diff = Integer.toString((int) ((endDate_.getTime() - currentDate.getTime()) / (24*60*60*1000)));
+	    } catch(Exception e) {
+	    	e.printStackTrace();
+	    }
+	    
+	    return diff;
+	}
+	
+}
