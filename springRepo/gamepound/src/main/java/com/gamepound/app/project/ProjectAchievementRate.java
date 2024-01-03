82eacba2cc076141930f16754e72f1ac3d4d70bf
DESKTOP-65C7M8P\user1 <clsrnclsrn1111@naver.com>
2024-01-03 15:53:49 +0900

    [chore] 달성률 메서드 static으로 변경

--- springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectAchievementRate.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectAchievementRate.java
@@ -3,7 +3,7 @@ package com.gamepound.app.project;
 public class ProjectAchievementRate {
 	
 	//프로젝트 달성률
-	public String achievementRate(String goalAmount, String currentAmount) {
+	static public String achievementRate(String goalAmount, String currentAmount) {
 		int goalA = Integer.parseInt(goalAmount);
 		int currentA = Integer.parseInt(currentAmount);
 		int result = (currentA*100)/goalA;
