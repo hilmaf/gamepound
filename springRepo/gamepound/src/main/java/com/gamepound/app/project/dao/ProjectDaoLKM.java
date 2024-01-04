2791d6e329b6b1d66b7b521351f35b19a316e49a
orlando <glee1470@naver.com>
2024-01-04 20:42:22 +0900

    [feat] 프로젝트 검색 기능

---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/dao/ProjectDaoLKM.java
@@ -0,0 +1,17 @@
+package com.gamepound.app.project.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.gamepound.app.project.vo.ProjectVo;
+
+@Repository
+public class ProjectDaoLKM {
+
+	public List<ProjectVo> searchProject(SqlSessionTemplate sst, String keyword) {
+		return sst.selectList("SearchMapper.searchProject", keyword);
+	}
+
+}
