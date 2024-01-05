674dbb8baf1bcffe6b283aa550be50dc945f7f5f
orlando <glee1470@naver.com>
2024-01-05 17:47:23 +0900

    [feat] 결제 기능 구현 중

--- springRepo/gamepound/src/main/java/com/gamepound/app/payment/controller/PaymentControllerLKM.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/payment/controller/PaymentControllerLKM.java
@@ -1,16 +1,28 @@
 package com.gamepound.app.payment.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.gamepound.app.payment.service.PaymentServiceLKM;
+import com.gamepound.app.payment.vo.PaymentVo;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("pay")
 public class PaymentControllerLKM {
 	
 	private final PaymentServiceLKM service;
 	
-	
+	// 결제하기
+	@Scheduled(cron = "0 0 0 * * *")
+	@PostMapping("")
+	public void pay(PaymentVo vo) {
+		
+		service.pay(vo);
+		
+	}
 }
