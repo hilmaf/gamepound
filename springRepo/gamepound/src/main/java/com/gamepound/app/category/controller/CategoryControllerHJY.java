555068ded4c481f8aa3126f9a914656e2d83d729
hdelay <102139789+hdelay@users.noreply.github.com>
2024-01-07 18:08:01 +0900

    [feat] 카테고리 조회 기능 추가

--- springRepo/gamepound/src/main/java/com/gamepound/app/category/controller/CategoryControllerHJY.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/category/controller/CategoryControllerHJY.java
@@ -1,11 +1,33 @@
 package com.gamepound.app.category.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gamepound.app.category.service.CategoryServiceHJY;
+import com.gamepound.app.category.vo.CategoryVo;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RequestMapping("category")
 @RequiredArgsConstructor
+@CrossOrigin("*")
+@RestController
 public class CategoryControllerHJY {
-
+	
+	private final CategoryServiceHJY service;
+	
+	// 카테고리 조회
+	@GetMapping("list")
+	public ArrayList<CategoryVo> getCategory() {
+		ArrayList<CategoryVo> result = service.getCategory();
+		return result;
+	}
+	
 }
--- springRepo/gamepound/src/main/java/com/gamepound/app/category/vo/SubCategoryVo.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/category/vo/SubCategoryVo.java
@@ -1,17 +1,12 @@
 package com.gamepound.app.category.vo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import lombok.Data;
 
 @Data
-public class CategoryVo {
+public class SubCategoryVo {
 	private String no;
 	private String mainCategoryNo;
 	private String subCategoryNo;
 	private String mainCategory;
 	private String subCategory;
-	
-	private List<SubCategoryVo> subCategoryList = new ArrayList<SubCategoryVo>();
 }
