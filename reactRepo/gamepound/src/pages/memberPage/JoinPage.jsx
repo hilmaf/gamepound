44be9ffacd82d1d84aa6b3bd3dd244e8752b2541
hdelay <102139789+hdelay@users.noreply.github.com>
2024-01-07 22:14:34 +0900

    [design] 회원가입,비밀번호찾기,로그인,새비밀번호입력,탈퇴 컴포넌트 생성

---
+++ reactRepo/gamepound/src/pages/memberPage/JoinPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const JoinPage = () => {
+    return (
+        <div>
+            회원가입페이지
+        </div>
+    );
+};
+
+export default JoinPage;
---
+++ reactRepo/gamepound/src/pages/memberPage/NewPwdPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewPwdPage = () => {
+    return (
+        <div>
+            새 비밀번호 설정 페이지
+        </div>
+    );
+};
+
+export default NewPwdPage;
