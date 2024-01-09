497255abaacc8c81364eb690dfce87710b5952cc
hyunDelay <clsrnclsrn1111@naver.com>
2024-01-09 17:37:05 +0900

    [chore] 리액트 화면 연결을 위한 map 응답 수정

--- springRepo/gamepound/src/main/java/com/gamepound/app/member/service/MailSendService.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/member/service/MailSendService.java
@@ -1,5 +1,7 @@
 package com.gamepound.app.member.service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.mail.MessagingException;
@@ -29,7 +31,7 @@ public class MailSendService {
     }
 	
 	// 이메일 인증번호 보내기
-	public int joinEmail(String email) {
+	public Map<String, Object> joinEmail(String email) {
 		
 		int verificationCode = makeRandomNumber();
 		
@@ -38,14 +40,17 @@ public class MailSendService {
 		String title = "[gamepound] 이메일 인증번호 입니다."; // 이메일 제목
 		String content = "[인증번호] " + verificationCode; // 이메일 내용
 		
-		mailSend(setFrom, toMail, title, content);
+		Map<String, Object> resultMap = mailSend(setFrom, toMail, title, content);
 		
-		return verificationCode;
+		resultMap.put("verificationCode", verificationCode);
+		
+		return resultMap;
 	}
 	
 	// 이메일 전송
-    public void mailSend(String setFrom, String toMail, String title, String content) {
+    public Map<String, Object> mailSend(String setFrom, String toMail, String title, String content) {
         MimeMessage message = mailSender.createMimeMessage();
+        Map<String, Object> map = new HashMap<>();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8"); // 이메일 메시지와 관련된 설정을 수행합니다.
             // true를 전달하여 multipart 형식의 메시지를 지원하고, "utf-8"을 전달하여 문자 인코딩을 설정
@@ -54,10 +59,14 @@ public class MailSendService {
             helper.setSubject(title);
             helper.setText(content, true); // 이메일의 내용 설정 두 번째 매개 변수에 true를 설정하여 html 설정으로한다.
             mailSender.send(message);
+            map.put("msg", "good");
             System.out.println("메일보내기 성공");
+            return map;
         } catch (MessagingException e) { 
         	// 이메일 서버에 연결할 수 없거나, 잘못된 이메일 주소를 사용하거나, 인증 오류가 발생하는 등 오류
+            map.put("msg", "bad");
             e.printStackTrace();
+            return map;
         }
 
     }
