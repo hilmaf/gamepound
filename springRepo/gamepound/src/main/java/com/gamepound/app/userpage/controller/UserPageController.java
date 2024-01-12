d7653cacc3e63fbef0e1e22cad06f55fb446b612
orlando <glee1470@naver.com>
2024-01-12 17:34:22 +0900

    [feat] 유저 프로필 소개 조회 기능 추가

--- springRepo/gamepound/src/main/java/com/gamepound/app/userpage/controller/UserPageController.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/userpage/controller/UserPageController.java
@@ -1,12 +1,17 @@
 package com.gamepound.app.userpage.controller;
 
+import java.nio.charset.Charset;
 import java.util.Map;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamepound.app.review.vo.ReviewVo;
@@ -22,13 +27,31 @@ public class UserPageController {
 	
 	private final UserPageService service;
 	
+	// 유저페이지 - 프로필
+	@GetMapping("")
+	public ResponseEntity<String> userProfile(@RequestParam("user") String memberNo) {
+		String profile = service.userProfile(memberNo);
+		
+		HttpHeaders header = getHttpHeaders("String");
+		
+		try {
+			if(profile!=null) {
+				return ResponseEntity.ok().headers(header).body(profile);
+			} else {
+				return ResponseEntity.notFound().headers(header).build();
+			}
+		} catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).headers(header).body("서버 내부 에러 500");
+		}
+	}
+	
 	// 유저페이지 - 리뷰목록, 리뷰 통계 조회
 	@GetMapping("review")
-	public void listReview(String memberNo) {
+	public Map<String, Object> listReview(@RequestParam("user") String memberNo) {
 		
 		Map<String, Object> map = service.listReview(memberNo);
 		
-		System.out.println(map);
+		return map;
 	}
 	
 	// 리뷰 작성하기
@@ -45,23 +68,27 @@ public class UserPageController {
 	}
 	
 	// 유저페이지 - 내가 올린 프로젝트 목록 조회
-	@PostMapping("created")
-	public Map<String, Object> listMyProjects(@RequestBody String memberNo) {
-		System.out.println(memberNo);
-		
+	@GetMapping("created")
+	public Map<String, Object> listMyProjects(@RequestParam("user") String memberNo) {
 		Map<String, Object> listMap = service.listMyProjects(memberNo);
 		
 		return listMap;
 	}
 	
 	// 유저페이지 - 내 후원 목록 조회
-	@PostMapping("backed")
-	public Map<String, Object> listMyBackedProjects(@RequestBody String memberNo) {
-		System.out.println(memberNo);
+	@GetMapping("backed")
+	public Map<String, Object> listMyBackedProjects(@RequestParam("user") String memberNo) {
 		Map<String, Object> map = service.listMyBackedProjects(memberNo);
 		
-		System.out.println(map);
-		
 		return map;
+	}
+	
+	// HttpHeaders 세팅
+	private HttpHeaders getHttpHeaders(String respType) {
+		
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(new MediaType("application", respType, Charset.forName("UTF-8")));;
+		
+		return header;
 	}
 }
