7724b26b1398e90312b8bf12064260dc325b85d0
orlando <glee1470@naver.com>
2024-01-12 17:33:01 +0900

    [fix] 데이터 변환 인터페이스 사용 작업중

--- springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectAchievementRate.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectAchievementRate.java
@@ -1,14 +1,25 @@
 package com.gamepound.app.project;
 
-public class ProjectAchievementRate {
+public class ProjectAchievementRate implements ProjectDataTransformation {
 	
 	//프로젝트 달성률
+	@Override
+	public String transform(String goalAmount, String currentAmount) {
+		int goalAmount_ = Integer.parseInt(removeCommas(goalAmount));
+		int currentAmount_ = Integer.parseInt(removeCommas(currentAmount));
+		
+		// 달성률 계산
+		int result = (currentAmount_*100)/goalAmount_;
+		
+		return Integer.toString(result);
+	}
+	
 	static public String achievementRate(String goalAmount, String currentAmount) {
 		ProjectAchievementRate par = new ProjectAchievementRate();
 		
 		// amount 가공
-		int goalA = Integer.parseInt(par.processAmount(goalAmount));
-		int currentA = Integer.parseInt(par.processAmount(currentAmount));
+		int goalA = Integer.parseInt(par.removeCommas(goalAmount));
+		int currentA = Integer.parseInt(par.removeCommas(currentAmount));
 		
 		// 달성률 계산
 		int result = (currentA*100)/goalA;
@@ -17,7 +28,7 @@ public class ProjectAchievementRate {
 	}
 	
 	// amount 문자열 가공
-	private String processAmount(String amount) {
-		return amount.replace(",", "");
+	private String removeCommas(String goalAmount) {
+		return goalAmount.replace(",", "");
 	}
 }
---
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/ProjectDataTransformation.java
@@ -0,0 +1,7 @@
+package com.gamepound.app.project;
+
+public interface ProjectDataTransformation {
+	
+	String transform(String input1, String input2);
+	
+}
