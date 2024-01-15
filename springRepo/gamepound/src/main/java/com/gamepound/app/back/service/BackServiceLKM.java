c820092c59dcca1088aedad3ee72242d8175bef5
orlando <glee1470@naver.com>
2024-01-15 17:39:16 +0900

    [feat] 후원하기 유효성 체크 service layer

--- springRepo/gamepound/src/main/java/com/gamepound/app/back/service/BackServiceLKM.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/back/service/BackServiceLKM.java
@@ -38,47 +38,90 @@ public class BackServiceLKM {
 		
 	}
 	
+	/**
+	 * BACKER 테이블에 INSERT
+	 * PAYMENT 테이블에 INSERT
+	 * CARD 테이블에 INSERT 또는 KAKAOPAY 테이블에 INSERT
+	 */
 	// 후원하기
 	public boolean back(BackDetailVo vo) throws Exception {
-		// NullCheck
+		// 결제수단 not null
 		if(vo.getPaymentType() == null) {
 			throw new Exception("결제수단 선택 필요");
 		}
-		
-		// TODO: cardNo 유효성 체크: not null, max length 2, 숫자
-		
-		// TODO: validThru 유효성 체크: not null, max length 2, 숫자
-		
-		// TODO: cardPwd 유효성 체크: not null, max length 2, 숫자
-		
-		// TODO: birthDate 유효성 체크: not null, max length 6, 숫자
-		
+
+		// paymentType no setting
+		if(vo.getPaymentType() == "card") {	
+			vo = validateCardInfo(vo);
+		} else {
+			// TODO: customerUid 유효성 체크
+			
+			vo.setPaymentTypeNo("2");
+		}
 		
 		// rewardAmount 수정
 		vo.setRewardAmount(util.removeCommas(vo.getRewardAmount()));
-		// paymentAmount setting
-		vo.setPaymentAmount(vo.getRewardAmount());
+		// paymentAmount 세팅
+		vo.setPaymentAmount(vo.getRewardAmount());		
 		
-		// paymentType no setting
-		if(vo.getPaymentType() == "card") {
-			vo.setPaymentTypeNo("1");
-		} else {
-			vo.setPaymentTypeNo("1");
-		}
 		
-		// 후원정보에 insert
 		int result1 = dao.insertBack(sst, vo);
-		
-		// 결제정보에 insert
 		int result2 = dao.insertPayment(sst, vo);
-		
-		if(result1 != 1 && result2 != 1) {
-			throw new Exception("후원 등록에 실패함");
+		int result3 = 0;
+		if("1".equals(vo.getPaymentType())) {
+			// 카드 정보 테이블 insert
+			result3 = dao.insertCard(sst, vo);			
+		} else {
+			// 카카오페이 정보 테이블 insert
+			result3 = dao.insertKakaopay(sst, vo);	
 		}
 
-		boolean backed = true;
+		if(result1 == 1 && result2 == 1 && result3 == 1) {
+			return true;
+		} else {
+			return false;
+		}
 		
-		return backed;
+	}
+	
+	private BackDetailVo validateCardInfo(BackDetailVo vo) throws Exception {
+		// 숫자only 정규표현식
+		String numRegex = "\\d+";
+		
+		
+		// 카드번호 유효성 체크 not null, max length 16, 숫자
+		String[] cardNos = {vo.getCardNo1(), vo.getCardNo2(), vo.getCardNo3(), vo.getCardNo4()};
+		String cardNo = util.compileInputs(cardNos);
+		
+		if(!cardNo.matches(numRegex) && (cardNo == null || cardNo.length() == 0 || cardNo.length() > 16)) {
+			throw new Exception("카드번호 값 오류");
+		}
+	
+		// 카드 유효기간 유효성 체크 not null, max length 4, 숫자
+		String[] validThrus = {vo.getValidThru1(), vo.getValidThru2()};
+		String validThru = util.compileInputs(validThrus);
+		
+		if(!validThru.matches(numRegex) && (validThru == null || validThru.length() == 0 || validThru.length() > 4)) {
+			throw new Exception("카드 유효기간 값 오류");
+		}
+		
+		// cardPwd 유효성 체크: not null, max length 2, 숫자
+		String cardPwd = vo.getCardPwd();
+		if(!cardPwd.matches(numRegex) && (cardPwd == null || cardPwd.length() == 0 || cardPwd.length() > 2)) {
+			throw new Exception("카드 비밀번호 값 오류");
+		}
+		
+		// birthDate 유효성 체크: not null, max length 6, 숫자
+		String birthDate = vo.getBirthDate();
+		if(!birthDate.matches(numRegex) && (birthDate == null || validThru.length() == 0 || validThru.length() > 4)) {
+			throw new Exception("생년월일 오류");
+		}
+				
+		vo.setPaymentTypeNo("1");
+		vo.setCardNo(cardNo);
+		vo.setValidThru(validThru);
+		
+		return vo;
 	}
 
 	// 후원완료(n번째 후원자)
