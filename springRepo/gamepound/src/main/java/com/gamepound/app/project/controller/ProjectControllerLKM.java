e3bba52b21acb25359ca2eda998a72a5a7665424
orlando <glee1470@naver.com>
2024-01-16 09:05:05 +0900

    [feat] 프로젝트 검색 구현 시작

--- springRepo/gamepound/src/main/java/com/gamepound/app/project/controller/ProjectControllerLKM.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/controller/ProjectControllerLKM.java
@@ -3,6 +3,7 @@ package com.gamepound.app.project.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamepound.app.project.service.ProjectServiceLKM;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("project")
 public class ProjectControllerLKM {
 
 	private final ProjectServiceLKM service;
