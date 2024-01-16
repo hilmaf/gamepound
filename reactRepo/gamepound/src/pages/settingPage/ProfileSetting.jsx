a7c3675293908f80539a44f3348016898aa92c12
DESKTOP-65C7M8P\user1 <gksdbswl951@gmail.com>
2024-01-16 14:27:35 +0900

    [file] 프로필 관련 페이지 생성

---
+++ reactRepo/gamepound/src/pages/settingPage/ProfileSetting.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledAllDiv = styled.div`
+    width: 100%;
+`;
+
+
+const ProfileSetting = () => {
+    return (
+        <StyledAllDiv>
+            하하하하하
+        </StyledAllDiv>
+    );
+};
+
+export default ProfileSetting;
