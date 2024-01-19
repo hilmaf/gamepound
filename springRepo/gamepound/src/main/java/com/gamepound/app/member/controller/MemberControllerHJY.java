5cbade56dd3522fd0ba199e853133a304b6a718e
hyunDelay <clsrnclsrn1111@naver.com>
2024-01-19 16:06:12 +0900

    [chore] 메일보낼때 local주소 변경 및 멤버컨트롤러에 import정리

--- springRepo/gamepound/src/main/java/com/gamepound/app/member/controller/MemberControllerHJY.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/member/controller/MemberControllerHJY.java
@@ -1,21 +1,12 @@
 package com.gamepound.app.member.controller;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gamepound.app.member.service.MailSendService;
