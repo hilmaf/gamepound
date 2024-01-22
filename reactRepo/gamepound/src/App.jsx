89b24a90c76b68fa2be50b9858b53e08235099e6
orlando <glee1470@naver.com>
2024-01-22 17:38:58 +0900

    [fix] 모달 렌더링 요소 설정(setAppElement 코드 추가)

--- reactRepo/gamepound/src/App.jsx
+++ reactRepo/gamepound/src/App.jsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/reset.css';
 import { BrowserRouter } from 'react-router-dom';
 import Wrap from './component/common/Wrap';
+import ReactModal from 'react-modal';
+
+ReactModal.setAppElement('#root');
 
 function App() {
   return (
