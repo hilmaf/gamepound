c2f99b747e14a9cef0e115be6fc368d3a967331d
orlando <glee1470@naver.com>
2024-01-23 08:55:43 +0900

    [feat] 어드민 백엔드 페이지 기능 구현 완료

--- springRepo/gamepound/src/main/java/com/gamepound/app/project/dao/AdminProjectDaoLKM.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/project/dao/AdminProjectDaoLKM.java
@@ -6,17 +6,32 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.gamepound.app.project.vo.ProjectDetailVo;
+import com.gamepound.app.project.vo.ProjectSearchVo;
 import com.gamepound.app.project.vo.ProjectVo;
 
 @Repository
 public class AdminProjectDaoLKM {
 
 	public List<ProjectVo> list(SqlSessionTemplate sst) {
+		int offset = 0;
+		int limit = 0;
 		return sst.selectList("AdminProjectMapper.list");
 	}
 
 	public List<ProjectDetailVo> detail(SqlSessionTemplate sst, String projectNo) {
-		return sst.selectList("AdminProjectMapper.detail");
+		return sst.selectList("AdminProjectMapper.detail", projectNo);
+	}
+
+	public int approve(SqlSessionTemplate sst, String projectNo) {
+		return sst.update("AdminProjectMapper.approve", projectNo);
+	}
+
+	public int reject(SqlSessionTemplate sst, String projectNo) {
+		return sst.update("AdminProjectMapper.reject", projectNo);
+	}
+
+	public List<ProjectVo> search(SqlSessionTemplate sst, ProjectSearchVo vo) {
+		return sst.selectList("AdminProjectMapper.search", vo);
 	}
 
 }
