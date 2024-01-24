330a01630b4a77d0b5ebe0346dd94e960b54c24c
DESKTOP-65C7M8P\user1 <gksdbswl951@gmail.com>
2024-01-24 17:43:19 +0900

    [feat] 관리자 사용자관리 화면 구현

--- springRepo/gamepound/src/main/java/com/gamepound/app/member/service/AdminMemberServiceHYJ.java
+++ springRepo/gamepound/src/main/java/com/gamepound/app/member/service/AdminMemberServiceHYJ.java
@@ -1,6 +1,8 @@
 package com.gamepound.app.member.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,18 +11,40 @@ import org.springframework.stereotype.Service;
 import com.gamepound.app.member.dao.AdminMemberDaoHYJ;
 import com.gamepound.app.member.dao.MemberDaoHYJ;
 import com.gamepound.app.member.vo.MemberVo;
+import com.gamepound.app.page.vo.PageVo;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AdminMemberServiceHYJ {
 	private final AdminMemberDaoHYJ dao;
 	private final SqlSessionTemplate sst;
 	
 	//목록
-	public List<MemberVo> memberList(MemberVo vo) {
-		return dao.memberList(vo,sst);
+	public Map<String, Object> memberList(MemberVo vo, String pageNum) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		//조건에 맞는 게시글 수
+		int listCount = dao.listCnt(sst, vo);
+		
+		//페이징
+		if(pageNum == null) {
+			pageNum = "1";
+		}
+		int cutterntPage = Integer.parseInt(pageNum);
+		int pageLimit = 5;
+		int boardLimit = 10;
+		PageVo pvo = new PageVo(listCount, cutterntPage, pageLimit, boardLimit);
+		map.put("pvo", pvo);
+		
+		//조건에 맞는 게시글들
+		List<MemberVo> voList = dao.memberList(sst, vo, pvo);
+		map.put("voList", voList);
+		
+		return map;
 	}
 
 	//상세
