c821b378200824d2bb1653fadb71dc05e30784f9
DESKTOP-65C7M8P\user1 <gksdbswl951@gmail.com>
2024-01-30 15:49:48 +0900

    [design] 프로필 css수정

--- reactRepo/gamepound/src/pages/projectpage/detail/PrelaunchUpdatePage.jsx
+++ reactRepo/gamepound/src/pages/projectpage/detail/PrelaunchUpdatePage.jsx
@@ -55,6 +55,7 @@ const StyledUpdateDiv = styled.div`
                         width: 100%;
                         height: 100%;
                         object-fit: cover;
+                        border-radius: 50px;
                     }
                 }
             }
--- reactRepo/gamepound/src/pages/projectpage/detail/UpdatePage.jsx
+++ reactRepo/gamepound/src/pages/projectpage/detail/UpdatePage.jsx
@@ -63,6 +63,7 @@ const StyledUpdateDiv = styled.div`
                         width: 100%;
                         height: 100%;
                         object-fit: cover;
+                        border-radius: 50px;
                     }
                 }
             }
